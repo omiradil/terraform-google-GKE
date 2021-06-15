fc842a25552561960fcaf4e0e72931a6922d7bb6
omiradil <omiradil@gmail.com>
2021-06-15 18:32:29 +0000

    added GKE

---
+++ module/gke.tf
@@ -0,0 +1,11 @@
+module "app" {
+  source         = "../"
+  project        = "xwibraqguembkefy"
+  region         = "us-central1"
+  zone           = "us-central1-c"
+  cluster_name   = "my-gke-cluster"
+  machine_type   = "e2-medium"
+  node_count     = 1
+  node_pool_name = "my-node-pool"
+  preemptible    = true
+}
---
+++ terraform.auto.tfvars
@@ -0,0 +1,10 @@
+gke_config = {
+  project        = "kcahrzeljocmrous"
+  region         = "us-central1"
+  zone           = "us-central1-c"
+  cluster_name   = "my-gke-cluster"
+  machine_type   = "e2-medium"
+  node_count     = 1
+  node_pool_name = "my-node-pool"
+  preemptible    = true
+}
